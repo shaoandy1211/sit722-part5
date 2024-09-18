fd583aa31b56df02f12c74568c9f08182d87e093
Di Shao <shaoandy1211@gmail.com>
2024-09-18 14:14:18 +1000

    update

---
+++ scripts/terraform/variables.tf
@@ -0,0 +1,17 @@
+# Sets global variables for this Terraform project.
+
+variable app_name {
+  default = "andysit722"
+}
+
+variable location {
+  default = "australiasoutheast"
+}
+
+variable kubernetes_version {
+  default = "1.30.3"    
+}
+
+variable "resource_group_name" {
+  default = "deakinuni"
+}
